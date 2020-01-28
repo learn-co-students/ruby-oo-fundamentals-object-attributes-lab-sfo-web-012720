5d83762aca6b4feaa695048d7a2138d55a17dfdc
Raymond Michael Morales <michaelmorales53@gmail.com>
2020-01-28 22:30:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,29 @@ class Dog
     
   end
   
+
+  def name=(name)
+  
+    @name = name
+  
+  end
+  
+  def name 
+    
+    @name
+    
+  end
+
+  def breed=(breed)
+  
+    @breed = breed
+  
+  end
+  
+  def breed
+    
+    @breed
+    
+  end
+  
 end
